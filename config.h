90f26c9b1b0287c4ec3cf49083fe2f9c99bf9012
zhenbukeluosi <1928731412@qq.com>
2025-03-31 20:40:56 +0800

    Create config.h

---
+++ config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define BG_PICTRUE_PATH":/img/img/bg/6c69e78e9d12e41040ecc6bb59fae08.png"
+#define INTRO_BG_PICTRUE_PATH":/img/img/bg/instructionbackground.png"
+#define ICON_PATH":/img/img/bg/projectIcon.png"
+#define GAME_BG_PATH":/img/img/bg/GameBackGround(1).png"
+#define HP_PATH":/img/img/HP.png"
+
+#define BGM_GAMEPAGE""
+#define BGM_MAINPAGE""
+
+#define AUDIO_ATK""
+#define AUDIO_SATK""
+#define AUDIO_DEFENCE""
+
+#define STAY_IMG":/character/img/character/maincharacter.png"
+
+#endif//CONFIG_H
